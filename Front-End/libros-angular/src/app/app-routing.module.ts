ef56b1a90749155cd96a504a2fed13e5350aa3a3
eduarchilon <eduarchilon@outlook.es>
2022-10-01 13:07:43 -0300

    create header

--- Front-End/libros-angular/src/app/app-routing.module.ts
+++ Front-End/libros-angular/src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 const routes: Routes = [
-  {path: '', redirectTo: '/inicio', pathMatch: 'full'}, // para q cualquier direccion mande a inicio 
+ // {path: '', redirectTo: '/inicio', pathMatch: 'full'}, // para q cualquier direccion mande a inicio
   {path: 'inicio', component: InicioComponent},
-  
+
 
 ];
 
