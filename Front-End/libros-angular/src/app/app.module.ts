a08ecca3d9a04e2ed685724c42e7ebf3d74ef74a
Eduar <83920925+eduarchilon@users.noreply.github.com>
2022-10-02 18:18:53 -0300

    fix: module to clear newsletter

--- Front-End/libros-angular/src/app/app.module.ts
+++ Front-End/libros-angular/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { HomeComponent } from './home/home.component';
-import { NewsletterComponent } from './newsletter/newsletter.component';
 import { ProductosHomeComponent } from './productos-home/productos-home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
@@ -19,7 +18,6 @@ import { NuevoUsuarioComponent } from './components/nuevo-usuario/nuevo-usuario.
     AppComponent,
     InicioComponent,
     HomeComponent,
-    NewsletterComponent,
     ProductosHomeComponent,
     HeaderComponent,
     FooterComponent,
