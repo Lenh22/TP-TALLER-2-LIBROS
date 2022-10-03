51d1194241e077523782f89692dddc5cd6d239cf
eduarchilon <eduarchilon@outlook.es>
2022-10-03 01:01:39 -0300

    fix: product list

--- Front-End/libros-angular/src/app/productos-home/productos-home.component.ts
+++ Front-End/libros-angular/src/app/productos-home/productos-home.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductosService } from '../servicios/productos.service';
-import {ListaProductos} from '../modulos/DataProductos'
+import { ListaProductos } from '../modulos/DataProductos';
 
 @Component({
   selector: 'app-productos-home',
   templateUrl: './productos-home.component.html',
-  styleUrls: ['./productos-home.component.css']
+  styleUrls: ['./productos-home.component.css'],
 })
 export class ProductosHomeComponent implements OnInit {
-  productosNuevos: ListaProductos[]=[];
+  productosNuevos: ListaProductos[] = [];
 
-  constructor(private serviciosProductos:ProductosService) { }
+  constructor(private serviciosProductos: ProductosService) {}
 
   ngOnInit(): void {
-    this.serviciosProductos.productosNuevosHome()
-      .subscribe(arg => {
-        console.log(arg);
-        this.productosNuevos=arg
-        console.log(this.productosNuevos);
-      });
-    
+    this.serviciosProductos.productosNuevosHome().subscribe((arg) => {
+      console.log(arg);
+      this.productosNuevos = arg;
+      console.log(this.productosNuevos);
+    });
   }
 
+  numSequence(n: number): Array<number> {
+    return Array(n);
+  }
 }
