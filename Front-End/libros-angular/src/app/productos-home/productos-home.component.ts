b5f0a5e47e09a55fbc2d9af64d7dec51196f6603
eduarchilon <eduarchilon@outlook.es>
2022-10-03 23:20:55 -0300

    feat: add pagination

--- Front-End/libros-angular/src/app/productos-home/productos-home.component.ts
+++ Front-End/libros-angular/src/app/productos-home/productos-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductosService } from '../servicios/productos.service';
 import { ListaProductos } from '../modulos/DataProductos';
-
+const FILTER_PAG_REGEX = /[^0-9]/g;
 @Component({
   selector: 'app-productos-home',
   templateUrl: './productos-home.component.html',
@@ -9,6 +9,7 @@ import { ListaProductos } from '../modulos/DataProductos';
 })
 export class ProductosHomeComponent implements OnInit {
   productosNuevos: ListaProductos[] = [];
+  page:number = 1;
 
   constructor(private serviciosProductos: ProductosService) {}
 
@@ -22,5 +23,13 @@ export class ProductosHomeComponent implements OnInit {
 
   numSequence(n: number): Array<number> {
     return Array(n);
+  }
+
+  selectPage(page: string) {
+    this.page = parseInt(page, 10) || 1;
+  }
+
+  formatInput(input: HTMLInputElement) {
+    input.value = input.value.replace(FILTER_PAG_REGEX, '');
   }
 }
