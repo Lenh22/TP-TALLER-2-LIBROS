c43408fde8478e33cd411bb4892902220d55c2cd
Lean Alonso <lea.alonso99@gmail.com>
2023-01-31 22:49:08 -0300

    Cambios en environments

--- Front-End/libros-angular/src/environments/environment.ts
+++ Front-End/libros-angular/src/environments/environment.ts
@@ -13,6 +13,7 @@ export const environment = {
 
   // URL: '[Aqui ira la otra url]',
   firebaseConfig: {
+    production: false,
     apiKey: 'AIzaSyBjY5vSDi-0rg1VD8kXvxhDPaZU35YyDhE',
     authDomain: 'libreria-el-romano.firebaseapp.com',
     databaseURL: 'https://libreria-el-romano-default-rtdb.firebaseio.com',
@@ -21,6 +22,8 @@ export const environment = {
     messagingSenderId: '1011937829242',
     appId: '1:1011937829242:web:7ac97f6216c1e428d4b6c3',
     measurementId: 'G-VZ241G8N6C',
+    URL: 'https://6330b5d2cff0e7bf70e0cb48.mockapi.io/', // PRODUCTO, CARRITO, USUARIO, CATEGORIA
+    URL2: 'https://634060dfd1fcddf69cb7ac22.mockapi.io/', // FAVORITO , ADMINISTRADOR
   },
 };
 
